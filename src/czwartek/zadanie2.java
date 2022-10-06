da087ee7d3a7c9e20a756c5af21f97cb0c4b109d
Jan Kowalski <JanKowalski@gmail.com>
2022-10-06 09:01:13 +0200

    commit nr3

--- src/czwartek/zadanie2.java
+++ src/czwartek/zadanie2.java
@@ -1,9 +1,29 @@
 package czwartek;
 
+import java.util.Scanner;
+
 public class zadanie2 {
+    public static void main(String[] args) {
+        int x, y;
 
+        Scanner keyboard = new Scanner(System.in);
 
+        System.out.println("Podaj dzień");
+         int dzien = keyboard.nextInt();
 
+        System.out.println("Podaj miesiąc");
+        int miesiac = keyboard.nextInt();
 
+        System.out.println("Podaj rok");
+        int rok = keyboard.nextInt();
 
+            if(dzien * miesiac == rok){
+                System.out.println("To jest magiczna data");
+
+            }
+        else{
+            System.out.println("To nie jest magiczna data");
+
+        }
+    }
 }
